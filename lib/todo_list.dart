7d7a6ee78d4c2bd743c093c0be840ac76ebe6460
Hanzla50 <hanzlajaved50281@gmail.com>
2024-06-15 13:47:46 +0500

    s

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-class todo extends StatelessWidget {
-  const todo({super.key});
+class Todo extends StatelessWidget {
+  const Todo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Title(color: Colors.yellow, child: Text("TODO")),
+        title: Text("Todo List", style: TextStyle(color: Colors.yellow)),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.yellow),
+            onPressed: () {
+              // Add your add to-do item logic here
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: TextStyle(color: Colors.white54),
+                prefixIcon: Icon(Icons.search, color: Colors.white54),
+                filled: true,
+                fillColor: Colors.grey[800],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          // Add other widgets here, like the to-do list
+        ],
       ),
     );
   }
