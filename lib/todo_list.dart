762623fa9ff27b235c34779e389249c5f0e18e75
Hanzla50 <hanzlajaved50281@gmail.com>
2024-06-17 23:14:48 +0500

    add

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Todo(),
+  ));
+}
+
 class Todo extends StatelessWidget {
   const Todo({super.key});
 
@@ -13,7 +19,10 @@ class Todo extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add, color: Colors.yellow),
             onPressed: () {
-              // Add your add to-do item logic here
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddTodoScreen()),
+              );
             },
           ),
         ],
@@ -39,6 +48,92 @@ class Todo extends StatelessWidget {
           ),
           // Add other widgets here, like the to-do list
         ],
+      ),
+    );
+  }
+}
+
+class AddTodoScreen extends StatefulWidget {
+  @override
+  _AddTodoScreenState createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _dateTimeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Todo", style: TextStyle(color: Colors.yellow)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'Todo Text',
+                filled: true,
+                fillColor: Colors.grey[800],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _dateTimeController,
+              decoration: InputDecoration(
+                labelText: 'Date and Time',
+                filled: true,
+                fillColor: Colors.grey[800],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+              style: TextStyle(color: Colors.white),
+              onTap: () async {
+                FocusScope.of(context).requestFocus(new FocusNode());
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (pickedDate != null) {
+                  TimeOfDay? pickedTime = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  if (pickedTime != null) {
+                    DateTime pickedDateTime = DateTime(
+                      pickedDate.year,
+                      pickedDate.month,
+                      pickedDate.day,
+                      pickedTime.hour,
+                      pickedTime.minute,
+                    );
+                    _dateTimeController.text = pickedDateTime.toString();
+                  }
+                }
+              },
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Save the todo item logic here
+                Navigator.pop(context);
+              },
+              child: Text("Save"),
+            ),
+          ],
+        ),
       ),
     );
   }
